bf1e4eb0db947d691e63e6e3ab70745cd572ecf1
Ricardo Gosch <ricardogosch@gmail.com>
2018-08-12 12:59:01 -0300

    Setup FizzBuzz

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-// Main
+const FizzBuzz = (num) => {
+  if((num % 3 === 0) && (num % 5 === 0)) return 'FizzBuzz';
+  if(num % 3 === 0) return 'Fizz';
+  if(num % 5 === 0) return 'Buzz';
+
+  return num;
+};
+
+export default FizzBuzz;
